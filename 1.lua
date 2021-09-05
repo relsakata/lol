954771c0a929c6da77f19a1aef2b535a283706c0
AbstractKenz <abstract.kenz@gmail.com>
2021-09-05 16:28:21 +0700

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -2,17 +2,16 @@ _G.FirePP = true
 
 while wait(2) do
 if _G.FirePP then
-
 for i, v in pairs(game:GetService("Workspace").ItemSpawns.Meteors:GetDescendants()) do
     if v.Name == "Meteor" then
     if v then
      game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
     else
-    local HttpService, TPService = game:GetService"HttpService", game:GetService"TeleportService";
-       TPService:Teleport(game.PlaceId)
+    local TPService = game:GetService("TeleportService");
+       TPService:Teleport(game.PlaceId, game.Players.LocalPlayer)
     game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
-        syn.queue_on_teleport("loadstring(game:HttpGet(('https://raw.githubusercontent.com/AbstractKenz/lol/main/1.lua'),true))()")
+        syn.queue_on_teleport("loadstring(game:HttpGet(('https://raw.githubusercontent.com/AbstractKenz/lol/main/1.lua'), true))()")
     end
 end)
 end
